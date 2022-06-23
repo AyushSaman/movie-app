b932422d5a39a5dbd11ea0d86981e1ef758ca271
AyushSaman <98798724+AyushSaman@users.noreply.github.com>
2022-06-23 14:30:33 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const nextBtn = document.getElementById('next')
+const prevBtn = document.getElementById('prev')
+const pageNum = document.getElementById('pageNum')
+
+prevBtn.classList.add("disable")
+
+
+
+let page = 1;
+
+
+
+
+pageNum.innerHTML = page
+
+let API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${page}`
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+
+
+
+
+// Get initial movies
+
+
+getMovies(API_URL)
+
+
+
+
+nextBtn.addEventListener('click',()=>{
+    page++
+    prevBtn.classList.remove("disable")
+    pageNum.innerHTML = page
+   return getMovies(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${page}`
+   )
+})
+
+prevBtn.addEventListener('click',()=>{
+    page--
+    if(page <= 1){
+        prevBtn.classList.add("disable")
+    }
+    pageNum.innerHTML = page
+   return getMovies(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${page}`
+   )
+})
+
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = ``
+
+    movies.forEach(movie => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        <img src=${IMG_PATH + poster_path} alt="img">
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByName(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByName(vote){
+    if (vote >= 8) {
+        return 'green'
+    }else if ( vote >= 5){
+        return 'orange'
+    }else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+    if (searchTerm && searchTerm !== ""){
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ""
+    }else{
+        window.location.reload();
+    }
+})
